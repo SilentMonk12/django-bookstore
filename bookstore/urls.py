47447666e8db07e070edaa88e23bca3a30b55422
Varun Wadhwa <94689768+VarunWadhwa1@users.noreply.github.com>
2023-05-30 17:37:00 +0530

    Update urls.py

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -6,14 +6,14 @@ from django.conf.urls.static import static
 from shop.views import ProductsView
 
 urlpatterns = [
-                path('admin/', admin.site.urls),
-                path('cart/', include('cart.urls')),
-                path('orders/', include('orders.urls')),
-                path('about/', include('about.urls')),
-                path('', include('shop.urls', namespace="shop")),
-                path('accounts/', include('accounts.urls')),
-                path('contact/', include('contact.urls')),
-                path('', ProductsView.as_view(), name="products"),
+    path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls')),
+    path('orders/', include('orders.urls')),
+    path('about/', include('about.urls')),
+    path('', include('shop.urls', namespace="shop")),
+    path('accounts/', include('accounts.urls')),
+    path('contact/', include('contact.urls')),
+    path('', ProductsView.as_view(), name="products"),
 
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+              + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
