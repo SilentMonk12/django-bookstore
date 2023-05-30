f4e8299292eb0927099d3dc7e1b5617167dd04e4
Varun Wadhwa <94689768+VarunWadhwa1@users.noreply.github.com>
2023-05-30 17:17:36 +0530

    Update urls.py

--- about/urls.py
+++ about/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'about'
 
 urlpatterns = [
     path('', views.about_detail, name='about_detail'),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
    
