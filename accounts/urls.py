62a138d25a6dafe75f559cded37df20bac9e0a53
Varun Wadhwa <94689768+VarunWadhwa1@users.noreply.github.com>
2023-05-30 17:16:26 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('profile/', views.profile_user, name='profile'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password/', views.change_password, name='change_password'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
