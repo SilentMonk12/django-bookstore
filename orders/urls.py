19d0ffd59ac79630f4a5bafd22fd1e5e61666dce
Varun Wadhwa <94689768+VarunWadhwa1@users.noreply.github.com>
2023-05-30 17:19:51 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'orders'
 
 urlpatterns = [
     path('create/', views.order_create, name='order_create'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
