a29c5109cb8412614f07b8bbdad8a0c2f561c0ca
Varun Wadhwa <94689768+VarunWadhwa1@users.noreply.github.com>
2023-05-30 14:49:19 +0530

    Update wsgi.py

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
+is_prod = 'WEBSITE_HOSTNAME' in os.environ
+
+settings_module = 'bookstore.production' if is_prod else 'bookstore.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookstore.settings')
 
 application = get_wsgi_application()
